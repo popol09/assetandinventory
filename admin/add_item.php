1252232efd5fda5a86a48033227c917418760a27
John Paul Dela Rosa <delarosapauldelarosa@gmail.com>
2025-01-24 18:14:38 +0800

    January 24 2025

--- admin/add_item.php
+++ admin/add_item.php
@@ -17,14 +17,8 @@
         $unit = $_POST['unit'];
         $life_span = $_POST['life_span'];
 
-        // Database configuration
-        $servername = "localhost"; // Change to your server name
-        $username = "root"; // Change to your database username
-        $password = ""; // Change to your database password
-        $dbname = "inventory"; // Change to your database name
-
         // Create a new connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli("localhost", "root", "", "inventory");
 
         // Check the connection
         if ($conn->connect_error) {
